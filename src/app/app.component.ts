8b127acb1aac1a76f338f7aa6ef007850b780d61
mhaik36 <46237956+mhaik36@users.noreply.github.com>
2020-09-30 13:37:48 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+declare var $: any;
+import { Observable, Observer, fromEvent, merge } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'checkInternet';
+  textContent = ""
+  //function to check the internet connection
+    ngOnInit(){
+      this.initService()
+    }
+    checkOnlineStatus = async () => {
+      try {
+        const online = await fetch("https://192.168.1.49:4100/assets/images/i2i-logo-only-ideas-to-Innovation.png");
+        return online.status >= 200 && online.status < 300; // either true or false
+      } catch (err) {
+        return false; // definitely offline
+      }
+    };
+    initService() {
+      setInterval(async () => {
+        const result = await this.checkOnlineStatus();
+        console.log(result)
+
+        this.textContent = result ? "Đang online!" : "Đang offline!";
+      }, 3000); // probably too often, try 30000 for every 30 seconds
+    }
 }
